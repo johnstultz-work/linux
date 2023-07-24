992be09e52c1022b8b278e6e36192f6a6ec2963d
Peter Zijlstra <peterz@infradead.org>
2023-07-24 16:49:22 +0000

    sched: Add blocked_donor link to task for smarter mutex handoffs
    Add link to the task this task is proxying for, and use it so
    the mutex owner can do an intelligent hand-off of the mutex to
    the task that the owner is running on behalf.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Juri Lelli <juri.lelli@redhat.com>
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Connor O'Brien <connoro@google.com>
    [jstultz: This patch was split out from larger proxy patch]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v5:
    * Split out from larger proxy patch
    v6:
    * Moved proxied value from earlier patch to this one where it
      is actually used
    * Rework logic to check sched_proxy_exec() instead of using ifdefs
    * Moved comment change to this patch where it makes sense
    v7:
    * Use more descriptive term then "us" in comments, as suggested
      by Metin Kaya.
    * Minor typo fixup from Metin Kaya
    * Reworked proxied variable to prev_not_proxied to simplify usage
    v8:
    * Use helper for donor blocked_on_state transition
    v9:
    * Re-add mutex lock handoff in the unlock path, but only when we
      have a blocked donor
    * Slight reword of commit message suggested by Metin
    v18:
    * Add task_init initialization for blocked_donor, suggested by
      Suleiman
    
    Cc: Joel Fernandes <joelagnelf@nvidia.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Suleiman Souhlal <suleiman@google.com>
    Cc: kuyo chang <kuyo.chang@mediatek.com>
    Cc: hupu <hupu.gm@gmail.com>
    Cc: kernel-team@android.com

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1210,6 +1210,7 @@ struct task_struct {
 #endif
 
 	struct mutex			*blocked_on;	/* lock we're blocked on */
+	struct task_struct		*blocked_donor;	/* task that is boosting this task */
 	raw_spinlock_t			blocked_lock;
 #ifdef CONFIG_SCHED_PROXY_EXEC
 	enum blocked_on_state		blocked_on_state;
--- kernel/fork.c
+++ kernel/fork.c
@@ -2330,10 +2330,10 @@ __latent_entropy struct task_struct *copy_process(
 #endif
 
 	p->blocked_on = NULL; /* not blocked yet */
+	p->blocked_donor = NULL; /* nobody is boosting p yet */
 #ifdef CONFIG_SCHED_PROXY_EXEC
 	p->blocked_on_state = BO_RUNNABLE;
 #endif
-
 #ifdef CONFIG_BCACHE
 	p->sequential_io	= 0;
 	p->sequential_io_avg	= 0;
