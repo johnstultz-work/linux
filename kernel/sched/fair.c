c7c3f35b023d72789dbb0d5f8139d3a77ce9f4d6
Peter Zijlstra <peterz@infradead.org>
2023-07-24 20:32:17 +0000

    sched: Start blocked_on chain processing in find_proxy_task()
    Start to flesh out the real find_proxy_task() implementation,
    but avoid the migration cases for now, in those cases just
    deactivate the donor task and pick again.
    
    To ensure the donor task or other blocked tasks in the chain
    aren't migrated away while we're running the proxy, also tweak
    the fair class logic to avoid migrating donor or mutex blocked
    tasks.
    
    Cc: Joel Fernandes <joelagnelf@nvidia.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Suleiman Souhlal <suleiman@google.com>
    Cc: kernel-team@android.com
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Juri Lelli <juri.lelli@redhat.com>
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Connor O'Brien <connoro@google.com>
    [jstultz: This change was split out from the larger proxy patch]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v5:
    * Split this out from larger proxy patch
    v7:
    * Minor refactoring of core find_proxy_task() function
    * Minor spelling and corrections suggested by Metin Kaya
    * Dropped an added BUG_ON that was frequently tripped
    v8:
    * Fix issue if proxy_deactivate fails, we don't leave task
      BO_BLOCKED
    * Switch to WARN_ON from BUG_ON checks
    v9:
    * Improve comments suggested by Metin
    * Minor cleanups
    v11:
    * Previously we checked next==rq->idle && prev==rq->idle, but I
      think we only really care if next==rq->idle from find_proxy_task,
      as we will still want to resched regardless of what prev was.
    v12:
    * Commit message rework for selected -> donor rewording
    v13:
    * Address new delayed dequeue condition (deactivate donor for now)
    * Next to donor renaming in find_proxy_task
    * Improved comments for find_proxy_task
    * Rework for proxy_deactivate cleanup
    v14:
    * Fix build error from __mutex_owner() with CONFIG_PREEMPT_RT
    v15:
    * Reworks for moving blocked_on_state to later in the series
    v16:
    * Pull down fix from later in the series where a deactivated
      task could pass the (task_cpu(owner) == this_cpu) check
      then have it be activated on a different cpu, so it passes
      the on_rq check. Thus double check the values in the opposite
      order to make sure nothing slips by.
    * Add resched_idle label to simplify common exit path
    * Get rid of preserve_need_resched flag and rework per Peter's
      suggestion
    * Rework find_proxy_task() to use guard to cleanup the exit gotos
      as Peter suggested.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9407,6 +9407,7 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 	 * 3) cannot be migrated to this CPU due to cpus_ptr, or
 	 * 4) running (obviously), or
 	 * 5) are cache-hot on their current CPU.
+	 * 6) are blocked on mutexes (if SCHED_PROXY_EXEC is enabled)
 	 */
 	if ((p->se.sched_delayed) && (env->migration_type != migrate_load))
 		return 0;
@@ -9426,6 +9427,9 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 
 	/* Disregard percpu kthreads; they are where they need to be. */
 	if (kthread_is_per_cpu(p))
+		return 0;
+
+	if (task_is_blocked(p))
 		return 0;
 
 	if (!cpumask_test_cpu(env->dst_cpu, p->cpus_ptr)) {
@@ -9463,7 +9467,8 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 	/* Record that we found at least one task that could run on dst_cpu */
 	env->flags &= ~LBF_ALL_PINNED;
 
-	if (task_on_cpu(env->src_rq, p)) {
+	if (task_on_cpu(env->src_rq, p) ||
+	    task_current_donor(env->src_rq, p)) {
 		schedstat_inc(p->stats.nr_failed_migrations_running);
 		return 0;
 	}
@@ -9506,6 +9511,9 @@ static void detach_task(struct task_struct *p, struct lb_env *env)
 		schedstat_inc(env->sd->lb_hot_gained[env->idle]);
 		schedstat_inc(p->stats.nr_forced_migrations);
 	}
+
+	WARN_ON(task_current(env->src_rq, p));
+	WARN_ON(task_current_donor(env->src_rq, p));
 
 	deactivate_task(env->src_rq, p, DEQUEUE_NOCLOCK);
 	set_task_cpu(p, env->dst_cpu);
