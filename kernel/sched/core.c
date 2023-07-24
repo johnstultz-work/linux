63497fea799d710aa8aad8160301fd8b9c29b233
Peter Zijlstra <peterz@infradead.org>
2023-07-24 16:49:22 +0000

    sched: Add blocked_donor link to task for smarter mutex handoffs
    Add link to the task this task is proxying for, and use it so
    the mutex owner can do an intelligent hand-off of the mutex to
    the task that the owner is running on behalf.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Juri Lelli <juri.lelli@redhat.com>
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Connor O'Brien <connoro@google.com>
    [jstultz: This patch was split out from larger proxy patch]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v5:
    * Split out from larger proxy patch
    v6:
    * Moved proxied value from earlier patch to this one where it
      is actually used
    * Rework logic to check sched_proxy_exec() instead of using ifdefs
    * Moved comment change to this patch where it makes sense
    v7:
    * Use more descriptive term then "us" in comments, as suggested
      by Metin Kaya.
    * Minor typo fixup from Metin Kaya
    * Reworked proxied variable to prev_not_proxied to simplify usage
    v8:
    * Use helper for donor blocked_on_state transition
    v9:
    * Re-add mutex lock handoff in the unlock path, but only when we
      have a blocked donor
    * Slight reword of commit message suggested by Metin
    v18:
    * Add task_init initialization for blocked_donor, suggested by
      Suleiman
    v23:
    * Reworks for PROXY_WAKING approach suggested by PeterZ
    v25:
    * Simplified some logic now we don't have proxy_tag_curr()
    
    Cc: Joel Fernandes <joelagnelf@nvidia.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Suleiman Souhlal <suleiman@google.com>
    Cc: kuyo chang <kuyo.chang@mediatek.com>
    Cc: hupu <hupu.gm@gmail.com>
    Cc: kernel-team@android.com

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6843,7 +6843,17 @@ err_out:
  * Find runnable lock owner to proxy for mutex blocked donor
  *
  * Follow the blocked-on relation:
- *   task->blocked_on -> mutex->owner -> task...
+ *
+ *                ,-> task
+ *                |     | blocked-on
+ *                |     v
+ *  blocked_donor |   mutex
+ *                |     | owner
+ *                |     v
+ *                `-- task
+ *
+ * and set the blocked_donor relation, this latter is used by the mutex
+ * code to find which (blocked) task to hand-off to.
  *
  * Lock order:
  *
@@ -6987,6 +6997,7 @@ find_proxy_task(struct rq *rq, struct task_struct *donor, struct rq_flags *rf)
 		 * rq, therefore holding @rq->lock is sufficient to
 		 * guarantee its existence, as per ttwu_remote().
 		 */
+		owner->blocked_donor = p;
 	}
 
 	/* Handle actions we need to do outside of the guard() scope */
@@ -7146,6 +7157,7 @@ pick_again:
 	assert_balance_callbacks_empty(rq);
 	next = pick_next_task(rq, &rf);
 	rq_set_donor(rq, next);
+	next->blocked_donor = NULL;
 	if (sched_proxy_exec()) {
 		if (unlikely(next->blocked_on)) {
 			next = find_proxy_task(rq, next, &rf);
