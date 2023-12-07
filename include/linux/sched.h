a82331a97d8870337576c11feaa7f48655aa4665
John Stultz <jstultz@google.com>
2023-12-06 20:05:15 -0800

    sched: Migrate whole chain in proxy_migrate_task()
    Instead of migrating one task each time through find_proxy_task(),
    we can walk up the blocked_donor ptrs and migrate the entire
    current chain in one go.
    
    This was broken out of earlier patches and held back while the
    series was being stabilized, but I wanted to re-introduce it.
    
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v12:
    * Earlier this was re-using blocked_node, but I hit
      a race with activating blocked entities, and to
      avoid it introduced a new migration_node listhead
    v18:
    * Add init_task initialization of migration_node as suggested
      by Suleiman
    
    Cc: Joel Fernandes <joelagnelf@nvidia.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Suleiman Souhlal <suleiman@google.com>
    Cc: kuyo chang <kuyo.chang@mediatek.com>
    Cc: hupu <hupu.gm@gmail.com>
    Cc: kernel-team@android.com

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1243,6 +1243,7 @@ struct task_struct {
 	enum blocked_on_state		blocked_on_state;
 	struct mutex			*blocked_on;	/* lock we're blocked on */
 	struct task_struct		*blocked_donor;	/* task that is boosting this task */
+	struct list_head		migration_node;
 	raw_spinlock_t			blocked_lock;
 
 #ifdef CONFIG_DETECT_HUNG_TASK_BLOCKER
