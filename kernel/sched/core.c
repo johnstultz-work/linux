e734f9e64ddee66ddab6cfcdbed5180677a8f982
John Stultz <jstultz@google.com>
2024-11-04 17:00:42 -0800

    MAYBEFIX?: Remove lost-wakeup handling workaround

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7227,7 +7227,9 @@ find_proxy_task(struct rq *rq, struct task_struct *donor, struct rq_flags *rf)
 				goto out;
 			}
 
-			goto needs_return;
+			raw_spin_unlock(&p->blocked_lock);
+			raw_spin_unlock(&mutex->wait_lock);
+			return proxy_resched_idle(rq);
 		}
 
 		owner_cpu = task_cpu(owner);
@@ -7328,9 +7330,6 @@ find_proxy_task(struct rq *rq, struct task_struct *donor, struct rq_flags *rf)
 			 * So schedule rq->idle so that ttwu_runnable can get the rq lock
 			 * and mark owner as running.
 			 */
-			if (p->blocked_on_state == BO_WAKING)
-				goto needs_return;
-
 			raw_spin_unlock(&p->blocked_lock);
 			raw_spin_unlock(&mutex->wait_lock);
 			return proxy_resched_idle(rq);
@@ -7355,8 +7354,11 @@ find_proxy_task(struct rq *rq, struct task_struct *donor, struct rq_flags *rf)
 		 * so we can briefly schedule idle so we release the rq and
 		 * let the wakeup complete.
 		 */
-		if (p->blocked_on_state == BO_WAKING)
-			goto needs_return;
+		if (p->blocked_on_state == BO_WAKING) {
+			raw_spin_unlock(&p->blocked_lock);
+			raw_spin_unlock(&mutex->wait_lock);
+			return proxy_resched_idle(rq);
+		}
 
 		/*
 		 * OK, now we're absolutely sure @owner is on this
