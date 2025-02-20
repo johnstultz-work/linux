24c3e8ff383dfd864aebb0784220bfd76756785a
John Stultz <jstultz@google.com>
2025-02-19 17:53:18 -0800

    sched/locking: Add blocked_on_state to provide necessary tri-state for proxy return-migration
    As we add functionality to proxy execution, we may migrate a
    donor task to a runqueue where it can't run due to cpu affinity.
    Thus, we must be careful to ensure we return-migrate the task
    back to a cpu in its cpumask when it becomes unblocked.
    
    Thus we need more then just a binary concept of the task being
    blocked on a mutex or not.
    
    So add a blocked_on_state value to the task, that allows the
    task to move through BO_RUNNING -> BO_BLOCKED -> BO_WAKING
    and back to BO_RUNNING. This provides a guard state in
    BO_WAKING so we can know the task is no longer blocked
    but we don't want to run it until we have potentially
    done return migration, back to a usable cpu.
    
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v15:
    * Split blocked_on_state into its own patch later in the
      series, as the tri-state isn't necessary until we deal
      with proxy/return migrations
    v16:
    * Handle case where task in the chain is being set as
      BO_WAKING by another cpu (usually via ww_mutex die code).
      Make sure we release the rq lock so the wakeup can
      complete.
    * Rework to use guard() in find_proxy_task() as suggested
      by Peter
    v18:
    * Add initialization of blocked_on_state for init_task
    v19:
    * PREEMPT_RT build fixups and rework suggested by
      K Prateek Nayak
    v20:
    * Simplify one of the blocked_on_state changes to avoid extra
      PREMEPT_RT conditionals
    v21:
    * Slight reworks due to avoiding nested blocked_lock locking
    * Be consistent in use of blocked_on_state helper functions
    * Rework calls to proxy_deactivate() to do proper locking
      around blocked_on_state changes that we were cheating in
      previous versions.
    * Minor cleanups, some comment improvements
    
    Cc: Joel Fernandes <joelagnelf@nvidia.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Suleiman Souhlal <suleiman@google.com>
    Cc: kuyo chang <kuyo.chang@mediatek.com>
    Cc: hupu <hupu.gm@gmail.com>
    Cc: kernel-team@android.com

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2264,7 +2264,7 @@ static inline bool task_is_blocked(struct task_struct *p)
 	if (!sched_proxy_exec())
 		return false;
 
-	return !!p->blocked_on;
+	return !!p->blocked_on && p->blocked_on_state != BO_RUNNABLE;
 }
 
 static inline int task_on_cpu(struct rq *rq, struct task_struct *p)
