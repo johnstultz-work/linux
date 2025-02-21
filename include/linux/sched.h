4debba17a48ce310ffd846f2a6fe098d36be7a35
Peter Zijlstra <peterz@infradead.org>
2025-02-20 17:25:59 -0800

    locking/mutex: Rework task_struct::blocked_on
    Track the blocked-on relation for mutexes, to allow following this
    relation at schedule time.
    
       task
         | blocked-on
         v
       mutex
         | owner
         v
       task
    
    This all will be used for tracking blocked-task/mutex chains
    with the prox-execution patch in a similar fashion to how
    priority inheritance is done with rt_mutexes.
    
    For serialization, blocked-on is only set by the task itself
    (current). And both when setting or clearing (potentially by
    others), is done while holding the mutex::wait_lock.
    
    Cc: Joel Fernandes <joelagnelf@nvidia.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Suleiman Souhlal <suleiman@google.com>
    Cc: kernel-team@android.com
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    [minor changes while rebasing]
    Signed-off-by: Juri Lelli <juri.lelli@redhat.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Connor O'Brien <connoro@google.com>
    [jstultz: Fix blocked_on tracking in __mutex_lock_common in error paths]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v2:
    * Fixed blocked_on tracking in error paths that was causing crashes
    v4:
    * Ensure we clear blocked_on when waking ww_mutexes to die or wound.
      This is critical so we don't get circular blocked_on relationships
      that can't be resolved.
    v5:
    * Fix potential bug where the skip_wait path might clear blocked_on
      when that path never set it
    * Slight tweaks to where we set blocked_on to make it consistent,
      along with extra WARN_ON correctness checking
    * Minor comment changes
    v7:
    * Minor commit message change suggested by Metin Kaya
    * Fix WARN_ON conditionals in unlock path (as blocked_on might already
      be cleared), found while looking at issue Metin Kaya raised.
    * Minor tweaks to be consistent in what we do under the
      blocked_on lock, also tweaked variable name to avoid confusion
      with label, and comment typos, as suggested by Metin Kaya
    * Minor tweak for CONFIG_SCHED_PROXY_EXEC name change
    * Moved unused block of code to later in the series, as suggested
      by Metin Kaya
    * Switch to a tri-state to be able to distinguish from waking and
      runnable so we can later safely do return migration from ttwu
    * Folded together with related blocked_on changes
    v8:
    * Fix issue leaving task BO_BLOCKED when calling into optimistic
      spinning path.
    * Include helper to better handle BO_BLOCKED->BO_WAKING transitions
    v9:
    * Typo fixup pointed out by Metin
    * Cleanup BO_WAKING->BO_RUNNABLE transitions for the !proxy case
    * Many cleanups and simplifications suggested by Metin
    v11:
    * Whitespace fixup pointed out by Metin
    v13:
    * Refactor set_blocked_on helpers clean things up a bit
    v14:
    * Small build fixup with PREEMPT_RT
    v15:
    * Improve consistency of names for functions that assume blocked_lock
      is held, as suggested by Peter
    * Use guard instead of separate spinlock/unlock calls, also suggested
      by Peter
    * Drop blocked_on_state tri-state for now, as its not needed until
      later in the series, when we get to proxy-migration and return-
      migration.
    v16:
    * Clear blocked on before optimistic spinning

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1234,10 +1234,7 @@ struct task_struct {
 	struct rt_mutex_waiter		*pi_blocked_on;
 #endif
 
-#ifdef CONFIG_DEBUG_MUTEXES
-	/* Mutex deadlock detection: */
-	struct mutex_waiter		*blocked_on;
-#endif
+	struct mutex			*blocked_on;	/* lock we're blocked on */
 
 #ifdef CONFIG_DETECT_HUNG_TASK_BLOCKER
 	struct mutex			*blocker_mutex;
