043ed90fe612e8adb632a848f2c634cfae515a48
John Stultz <jstultz@google.com>
2025-02-21 19:34:36 -0800

    locking: Add task::blocked_lock to serialize blocked_on state
    So far, we have been able to utilize the mutex::wait_lock
    for serializing the blocked_on state, but when we move to
    proxying across runqueues, we will need to add more state
    and a way to serialize changes to this state in contexts
    where we don't hold the mutex::wait_lock.
    
    So introduce the task::blocked_lock, which nests under the
    mutex::wait_lock in the locking order, and rework the locking
    to use it.
    
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v15:
    * Split back out into later in the series
    v16:
    * Fixups to mark tasks unblocked before sleeping in
      mutex_optimistic_spin()
    * Rework to use guard() as suggested by Peter
    v19:
    * Rework logic for PREEMPT_RT issues reported by
      K Prateek Nayak
    
    Cc: Joel Fernandes <joelagnelf@nvidia.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Suleiman Souhlal <suleiman@google.com>
    Cc: kernel-team@android.com

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1232,6 +1232,7 @@ struct task_struct {
 #endif
 
 	struct mutex			*blocked_on;	/* lock we're blocked on */
+	raw_spinlock_t			blocked_lock;
 
 #ifdef CONFIG_DETECT_HUNG_TASK_BLOCKER
 	/*
@@ -2145,8 +2146,8 @@ static inline void __set_task_blocked_on(struct task_struct *p, struct mutex *m)
 	WARN_ON_ONCE(!m);
 	/* The task should only be setting itself as blocked */
 	WARN_ON_ONCE(p != current);
-	/* Currently we serialize blocked_on under the mutex::wait_lock */
-	lockdep_assert_held_once(&m->wait_lock);
+	/* Currently we serialize blocked_on under the task::blocked_lock */
+	lockdep_assert_held_once(&p->blocked_lock);
 	/*
 	 * Check ensure we don't overwrite existing mutex value
 	 * with a different mutex. Note, setting it to the same
@@ -2158,15 +2159,14 @@ static inline void __set_task_blocked_on(struct task_struct *p, struct mutex *m)
 
 static inline void set_task_blocked_on(struct task_struct *p, struct mutex *m)
 {
-	guard(raw_spinlock_irqsave)(&m->wait_lock);
+	guard(raw_spinlock_irqsave)(&p->blocked_lock);
 	__set_task_blocked_on(p, m);
 }
 
 static inline void __clear_task_blocked_on(struct task_struct *p, struct mutex *m)
 {
-	WARN_ON_ONCE(!m);
-	/* Currently we serialize blocked_on under the mutex::wait_lock */
-	lockdep_assert_held_once(&m->wait_lock);
+	/* Currently we serialize blocked_on under the task::blocked_lock */
+	lockdep_assert_held_once(&p->blocked_lock);
 	/*
 	 * There may be cases where we re-clear already cleared
 	 * blocked_on relationships, but make sure we are not
@@ -2178,8 +2178,15 @@ static inline void __clear_task_blocked_on(struct task_struct *p, struct mutex *
 
 static inline void clear_task_blocked_on(struct task_struct *p, struct mutex *m)
 {
-	guard(raw_spinlock_irqsave)(&m->wait_lock);
+	guard(raw_spinlock_irqsave)(&p->blocked_lock);
 	__clear_task_blocked_on(p, m);
+}
+
+static inline void clear_task_blocked_on_nested(struct task_struct *p, struct mutex *m)
+{
+	raw_spin_lock_nested(&p->blocked_lock, SINGLE_DEPTH_NESTING);
+	__clear_task_blocked_on(p, m);
+	raw_spin_unlock(&p->blocked_lock);
 }
 #else
 static inline void __clear_task_blocked_on(struct task_struct *p, struct rt_mutex *m)
@@ -2187,6 +2194,10 @@ static inline void __clear_task_blocked_on(struct task_struct *p, struct rt_mute
 }
 
 static inline void clear_task_blocked_on(struct task_struct *p, struct rt_mutex *m)
+{
+}
+
+static inline void clear_task_blocked_on_nested(struct task_struct *p, struct rt_mutex *m)
 {
 }
 #endif /* !CONFIG_PREEMPT_RT */
