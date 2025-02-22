d3a3a6b484fabe617726f160abd2c16925d2a344
John Stultz <jstultz@google.com>
2025-02-21 23:52:56 -0800

    FOLD: drop pick again zap_balance_callbacks
    This is sort of broken as no one is calling zap_balance_callbacks,
    so we need to rework the zap_balance_Callback logic to where its used

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7440,11 +7440,8 @@ pick_again:
 	next->blocked_donor = NULL;
 	if (unlikely(task_is_blocked(next))) {
 		next = find_proxy_task(rq, next, &rf);
-		if (!next) {
-			/* zap the balance_callbacks before picking again */
-			zap_balance_callbacks(rq);
+		if (!next)
 			goto pick_again;
-		}
 		if (next == rq->idle)
 			goto keep_resched;
 	}
