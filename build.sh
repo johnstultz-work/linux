97fad489e57a4fed70163d6c75f15605ee1c4a21
John Stultz <jstultz@google.com>
2025-07-09 04:19:13 +0000

    DEBUG: Tooling to help test build different defconfigs
    Add extra defconfigs and a build script to build them
    
    Try to avoid some of the build issues with different
    config options that I had missed earlier.
    
    Signed-off-by: John Stultz <jstultz@google.com>

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+parallel  --help | grep "GNU" > /dev/null
+if [ $? -ne 0 ]; then
+	echo "Need GNU version of parallel."
+	echo "Try 'apt-get install parallel'?"
+	exit -1;
+fi
+
+
+do_build() {
+	set +e
+	cfg=$1
+	echo "=====[Using $cfg...]====================="
+	mkdir -p out/$cfg > /dev/null
+	make O=out/$cfg  $cfg > /dev/null;
+	make O=out/$cfg -j20 bzImage > /dev/null ;
+	if [ $? -ne 0 ]; then
+		echo "=====[ERROR: Config $cfg didn't build!]==="
+		return -1;
+	else
+		echo "=====[$cfg build succeeded!]=============="
+	fi
+}
+
+export -f do_build
+
+set -e
+
+CFGS="proxy_test_defconfig noproxy_test_defconfig proxy_nosmp_test_defconfig proxy_preempt_test_defconfig"
+
+date;
+git log --pretty="Building %C(yellow)%h%C(reset) %s" HEAD~1..HEAD
+time parallel --halt-on-error 2 -j 0 --group do_build {} ::: $CFGS
+
