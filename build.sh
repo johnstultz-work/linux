a80a7ece1c8dbcf85370af9ea38674e6f78bfc77
John Stultz <jstultz@google.com>
2025-07-09 04:19:13 +0000

    DEBUG: Tooling to help test build different defconfigs
    Add extra defconfigs and a build script to build them
    
    Try to avoid some of the build issues with different
    config options that I had missed earlier.
    
    Signed-off-by: John Stultz <jstultz@google.com>

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+CFGS="proxy_test_defconfig proxy_nosmp_test_defconfig proxy_preempt_test_defconfig"
+
+date
+git log --pretty="Building %C(yellow)%h%C(reset) %s" HEAD~1..HEAD
+for cfg in $CFGS
+do 
+	echo "=====[Using $cfg...]====================="
+	mkdir -p out/$cfg
+	make O=out/$cfg  $cfg;
+	make O=out/$cfg -j80 bzImage > /dev/null ;
+done
+date
