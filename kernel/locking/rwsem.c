62a1f798a8b9c2578cbdbfa85b81ca28887563fd
Suleiman Souhlal <suleiman@google.com>
2025-09-25 11:41:53 +0900

    locking/rwsem: Make readers use blocked_on APIs when owned by writer.
    Make rwsem read blockers set their task_struct->blocked_on when
    blocked on a rwsem held by a writer, allowing the owner to potentially
    run as their proxy, analogously to writer blockers.
    
    Signed-off-by: Suleiman Souhlal <suleiman@google.com>
    [jstultz: Switch direct blocked_on_state access to use helper]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v24:
    * Remove unnecessary nested locked.
    * Reword comment from BO_* to PROXY_WAKING.
    * __clear_task_blocked_on in same blocked_lock acquisition instance
      as __set_task_blocked_on() to avoid PROXY_WAKING from getting set
      in between.

--- kernel/locking/rwsem.c
+++ kernel/locking/rwsem.c
@@ -587,6 +587,14 @@ static void rwsem_mark_wake(struct rw_semaphore *sem,
 		 * after setting the reader waiter to nil.
 		 */
 		wake_q_add_safe(wake_q, tsk);
+		raw_spin_lock(&tsk->blocked_lock);
+		/*
+		 * We also get here if sem was reader owned and thus
+		 * waiter isn't blocked_on, but this function only sets
+		 * PROXY_WAKING if we are.
+		 */
+		__set_task_blocked_on_waking(tsk, sem);
+		raw_spin_unlock(&tsk->blocked_lock);
 	}
 }
 
@@ -1022,6 +1030,7 @@ rwsem_down_read_slowpath(struct rw_semaphore *sem, long count, unsigned int stat
 	long rcnt = (count >> RWSEM_READER_SHIFT);
 	struct rwsem_waiter waiter;
 	DEFINE_WAKE_Q(wake_q);
+	bool blocked_on_set;
 
 	/*
 	 * To prevent a constant stream of readers from starving a sleeping
@@ -1091,8 +1100,17 @@ queue:
 
 	trace_contention_begin(sem, LCB_F_READ);
 
+	blocked_on_set = false;
 	/* wait to be given the lock */
 	for (;;) {
+		if (atomic_long_read(&sem->count) & RWSEM_WRITER_MASK) {
+			raw_spin_lock_irq(&current->blocked_lock);
+			/* PROXY_WAKE might have been set */
+			__clear_task_blocked_on(current, sem);
+			__set_task_blocked_on(current, sem, BO_T_RWSEM);
+			raw_spin_unlock_irq(&current->blocked_lock);
+			blocked_on_set = true;
+		}
 		set_current_state(state);
 		if (!smp_load_acquire(&waiter.task)) {
 			/* Matches rwsem_mark_wake()'s smp_store_release(). */
@@ -1111,6 +1129,8 @@ queue:
 	}
 
 	__set_current_state(TASK_RUNNING);
+	if (blocked_on_set)
+		clear_task_blocked_on(current, sem);
 	lockevent_inc(rwsem_rlock);
 	trace_contention_end(sem, 0);
 	return sem;
@@ -1118,6 +1138,8 @@ queue:
 out_nolock:
 	rwsem_del_wake_waiter(sem, &waiter, &wake_q);
 	__set_current_state(TASK_RUNNING);
+	if (blocked_on_set)
+		clear_task_blocked_on(current, sem);
 	lockevent_inc(rwsem_rlock_fail);
 	trace_contention_end(sem, -EINTR);
 	return ERR_PTR(-EINTR);
