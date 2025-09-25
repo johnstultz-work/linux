6812899dfc1a4a7181ecd47e5cf209bb851fef60
Suleiman Souhlal <suleiman@google.com>
2025-09-25 11:29:45 +0900

    locking/rwsem: Make rwsem write blockers use blocked_on APIs.
    Make rwsem write blockers set their task_struct->blocked_on when
    blocked on a rwsem held by another writer, allowing the owner to
    potentially run as their proxy.
    
    Does not affect readers or rwsems held by readers.
    
    Signed-off-by: Suleiman Souhlal <suleiman@google.com>
    [jstultz: Switched direct blocked_on_state access to use helper]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v23:
    * Rework for PROXY_WAKING approach

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -152,6 +152,8 @@ static inline struct task_struct *__blocked_on_owner(struct blocked_on_lock *bo)
 		if (bo->lock == PROXY_WAKING)
 			return NULL;
 		return __mutex_owner(bo->lock);
+	case BO_T_RWSEM:
+		return rwsem_writer_owner(bo->lock);
 	default:
 		BUG();
 	}
@@ -7198,6 +7200,8 @@ lock_blocked_on_lock(struct blocked_on_lock *bo)
 {
 	if (bo->type == BO_T_MUTEX)
 		raw_spin_lock(&((struct mutex *)bo->lock)->wait_lock);
+	else if (bo->type == BO_T_RWSEM)
+		raw_spin_lock(&((struct rw_semaphore *)bo->lock)->wait_lock);
 	else
 		BUG();
 }
@@ -7207,6 +7211,8 @@ unlock_blocked_on_lock(struct blocked_on_lock *bo)
 {
 	if (bo->type == BO_T_MUTEX)
 		raw_spin_unlock(&((struct mutex *)bo->lock)->wait_lock);
+	else if (bo->type == BO_T_RWSEM)
+		raw_spin_unlock(&((struct rw_semaphore *)bo->lock)->wait_lock);
 	else
 		BUG();
 }
