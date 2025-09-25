d7f1e64fc57515eafe87ffee8c138a5a39b1b0fc
Suleiman Souhlal <suleiman@google.com>
2025-09-25 11:29:45 +0900

    locking/rwsem: Make rwsem write blockers use blocked_on APIs.
    Make rwsem write blockers set their task_struct->blocked_on when
    blocked on a rwsem held by another writer, allowing the owner to
    potentially run as their proxy.
    
    Does not affect readers or rwsems held by readers.
    
    Signed-off-by: Suleiman Souhlal <suleiman@google.com>
    [jstultz: Switched direct blocked_on_state access to use helper]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v23:
    * Rework for PROXY_WAKING approach
    
    v24:
    * Reduce scope of blocked_lock in rwsem_down_write_slowpath().
      This also lets us avoid nested acquisition in rwsem_mark_wake().
    * Reword comment from BO_* to PROXY_WAKING.
    * Clear task blocked_on in rwsem_down_write_slowpath().
    * Minor fixup for double unlock on error path

--- kernel/locking/rwsem.c
+++ kernel/locking/rwsem.c
@@ -287,6 +287,23 @@ rwsem_owner_flags(struct rw_semaphore *sem, unsigned long *pflags)
 }
 
 /*
+ * Return the task struct pointer of the owner if it's a writer,
+ * NULL otherwise.
+ */
+struct task_struct *rwsem_writer_owner(struct rw_semaphore *sem)
+{
+	struct task_struct *owner;
+	long count, flags;
+
+	count = atomic_long_read(&sem->count);
+	owner = rwsem_owner_flags(sem, &flags);
+	if (!(count & RWSEM_WRITER_MASK) || (flags & (RWSEM_READER_OWNED |
+	    RWSEM_NONSPINNABLE)))
+		return NULL;
+	return owner;
+}
+
+/*
  * Guide to the rw_semaphore's count field.
  *
  * When the RWSEM_WRITER_LOCKED bit in count is set, the lock is owned
@@ -432,6 +449,14 @@ static void rwsem_mark_wake(struct rw_semaphore *sem,
 			 * Readers, on the other hand, will block as they
 			 * will notice the queued writer.
 			 */
+			raw_spin_lock(&waiter->task->blocked_lock);
+			/*
+			 * We also get here if sem was reader owned and thus
+			 * waiter isn't blocked_on, but this function only sets
+			 * PROXY_WAKING if it is.
+			 */
+			__set_task_blocked_on_waking(waiter->task, sem);
+			raw_spin_unlock(&waiter->task->blocked_lock);
 			wake_q_add(wake_q, waiter->task);
 			lockevent_inc(rwsem_wake_writer);
 		}
@@ -1112,6 +1137,7 @@ rwsem_down_write_slowpath(struct rw_semaphore *sem, int state)
 {
 	struct rwsem_waiter waiter;
 	DEFINE_WAKE_Q(wake_q);
+	bool blocked_on_set;
 
 	/* do optimistic spinning and steal lock if possible */
 	if (rwsem_can_spin_on_owner(sem) && rwsem_optimistic_spin(sem)) {
@@ -1148,9 +1174,15 @@ rwsem_down_write_slowpath(struct rw_semaphore *sem, int state)
 		atomic_long_or(RWSEM_FLAG_WAITERS, &sem->count);
 	}
 
+	raw_spin_lock(&current->blocked_lock);
 	/* wait until we successfully acquire the lock */
 	set_current_state(state);
 	trace_contention_begin(sem, LCB_F_WRITE);
+	blocked_on_set = false;
+	if (atomic_long_read(&sem->count) & RWSEM_WRITER_MASK) {
+		__set_task_blocked_on(current, sem, BO_T_RWSEM);
+		blocked_on_set = true;
+	}
 
 	if (state == TASK_UNINTERRUPTIBLE)
 		hung_task_set_blocker(sem, BLOCKER_TYPE_RWSEM_WRITER);
@@ -1161,6 +1193,9 @@ rwsem_down_write_slowpath(struct rw_semaphore *sem, int state)
 			break;
 		}
 
+		if (blocked_on_set && !__get_task_blocked_on(current))
+			__set_task_blocked_on(current, sem, BO_T_RWSEM);
+		raw_spin_unlock(&current->blocked_lock);
 		raw_spin_unlock_irq(&sem->wait_lock);
 
 		if (signal_pending_state(state, current))
@@ -1187,12 +1222,18 @@ rwsem_down_write_slowpath(struct rw_semaphore *sem, int state)
 		set_current_state(state);
 trylock_again:
 		raw_spin_lock_irq(&sem->wait_lock);
+		raw_spin_lock(&current->blocked_lock);
+		if (blocked_on_set)
+			__clear_task_blocked_on(current, sem);
 	}
 
 	if (state == TASK_UNINTERRUPTIBLE)
 		hung_task_clear_blocker();
 
 	__set_current_state(TASK_RUNNING);
+	if (blocked_on_set)
+		__clear_task_blocked_on(current, sem);
+	raw_spin_unlock(&current->blocked_lock);
 	raw_spin_unlock_irq(&sem->wait_lock);
 	lockevent_inc(rwsem_wlock);
 	trace_contention_end(sem, 0);
@@ -1200,6 +1241,8 @@ trylock_again:
 
 out_nolock:
 	__set_current_state(TASK_RUNNING);
+	if (blocked_on_set)
+		clear_task_blocked_on(current, sem);
 	raw_spin_lock_irq(&sem->wait_lock);
 	rwsem_del_wake_waiter(sem, &waiter, &wake_q);
 	lockevent_inc(rwsem_wlock_fail);
