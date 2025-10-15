ea9c569d313ff078a55573c33f0b2b97eb487b72
John Stultz <jstultz@google.com>
2025-10-15 09:00:41 +0000

    sched: Add assert_balance_callbacks_empty helper
    With proxy-exec utilizing pick-again logic, we can end up having
    balance callbacks set by the preivous pick_next_task() call left
    on the list.
    
    So pull the warning out into a helper function, and make sure we
    check it when we pick again.
    
    Suggested-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    Cc: Joel Fernandes <joelagnelf@nvidia.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Suleiman Souhlal <suleiman@google.com>
    Cc: kuyo chang <kuyo.chang@mediatek.com>
    Cc: hupu <hupu.gm@gmail.com>
    Cc: kernel-team@android.com

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1734,6 +1734,15 @@ struct rq_flags {
 
 extern struct balance_callback balance_push_callback;
 
+#if defined(CONFIG_PROVE_LOCKING) && defined(CONFIG_SMP)
+static inline void assert_balance_callbacks_empty(struct rq *rq)
+{
+	WARN_ON_ONCE(rq->balance_callback && rq->balance_callback != &balance_push_callback);
+}
+#else
+static inline void assert_balance_callbacks_empty(struct rq *rq) {}
+#endif
+
 /*
  * Lockdep annotation that avoids accidental unlocks; it's like a
  * sticky/continuous lockdep_assert_held().
@@ -1752,7 +1761,7 @@ static inline void rq_pin_lock(struct rq *rq, struct rq_flags *rf)
 	rq->clock_update_flags &= (RQCF_REQ_SKIP|RQCF_ACT_SKIP);
 	rf->clock_update_flags = 0;
 # ifdef CONFIG_SMP
-	SCHED_WARN_ON(rq->balance_callback && rq->balance_callback != &balance_push_callback);
+	assert_balance_callbacks_empty(rq);
 # endif
 #endif
 }
