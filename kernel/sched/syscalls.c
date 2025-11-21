8577ade264c34655fb88ddf40c94ae59d225e716
John Stultz <jstultz@google.com>
2025-11-21 01:47:47 +0000

    sched: Avoid donor->sched_class->yield_task() null traversal
    With proxy-exec once we do return migration from ttwu(), if a
    task is proxying for a waiting donor, and the donor is woken up,
    we switch the rq->donor to point to idle briefly until we can
    re-enter __schedule().
    
    However, if a task that was acting as a proxy calls into
    yield() right after the donor is switched to idle, it may
    trip a null  pointer traversal, because the idle task doesn't
    have a yield_task() pointer.
    
    So add a conditional to ensure we don't try to call the
    yield_task() pointer in that case.
    
    This was only recently found because prior to commit
    127b90315ca07 ("sched/proxy: Yield the donor task")
    do_sched_yield() incorrectly called
    current->sched_class_yield_task() instead of using
    rq->donor.
    
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    Cc: Joel Fernandes <joelagnelf@nvidia.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Suleiman Souhlal <suleiman@google.com>
    Cc: kuyo chang <kuyo.chang@mediatek.com>
    Cc: hupu <hupu.gm@gmail.com>
    Cc: kernel-team@android.com

--- kernel/sched/syscalls.c
+++ kernel/sched/syscalls.c
@@ -1351,7 +1351,8 @@ static void do_sched_yield(void)
 	rq = this_rq_lock_irq(&rf);
 
 	schedstat_inc(rq->yld_count);
-	rq->donor->sched_class->yield_task(rq);
+	if (rq->donor->sched_class->yield_task)
+		rq->donor->sched_class->yield_task(rq);
 
 	preempt_disable();
 	rq_unlock_irq(rq, &rf);
