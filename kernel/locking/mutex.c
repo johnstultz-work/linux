d3827fb83605394e3e8fa2bfbcd7c62813cd2295
John Stultz <jstultz@google.com>
2026-01-13 23:15:36 +0000

    Revert "MAYBEFIX: locking/mutex: Optimization for donor handoff"
    This reverts commit 3684c3d2cdcd81489786f1ce1f1c5f786f9e4536.

--- kernel/locking/mutex.c
+++ kernel/locking/mutex.c
@@ -930,7 +930,6 @@ static noinline void __sched __mutex_unlock_slowpath(struct mutex *lock, unsigne
 	DEFINE_WAKE_Q(wake_q);
 	unsigned long owner;
 	unsigned long flags;
-	bool handoff_preempt = false;
 
 	mutex_release(&lock->dep_map, ip);
 
@@ -984,7 +983,6 @@ static noinline void __sched __mutex_unlock_slowpath(struct mutex *lock, unsigne
 				__set_task_blocked_on_waking(donor, next_lock);
 				wake_q_add(&wake_q, donor);
 				current->blocked_donor = NULL;
-				handoff_preempt = true;
 			}
 			raw_spin_unlock(&donor->blocked_lock);
 		}
@@ -1014,9 +1012,6 @@ static noinline void __sched __mutex_unlock_slowpath(struct mutex *lock, unsigne
 
 	raw_spin_unlock(&current->blocked_lock);
 	raw_spin_unlock_irqrestore_wake(&lock->wait_lock, flags, &wake_q);
-	/* If we were proxying, preempt to allow us to switch to donor */
-	if (handoff_preempt)
-		preempt_schedule();
 }
 
 #ifndef CONFIG_DEBUG_LOCK_ALLOC
