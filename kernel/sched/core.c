5c8abc82e17f64bd4e1730ca5143fd3858d996dc
K Prateek Nayak <kprateek.nayak@amd.com>
2026-07-17 11:16:01 +0000

    sched/core: Swap to lock owner for core-wide pick on core-cookie mismatch
    Proxy donor can have a different core cookie compared to the lock owner.
    Simply running a lock owner with a mismatched core-cookie in not an
    option as this breaks the core scheduling guarantees.
    
    To prevent a cookie mismatch, use the "rq->core_pick_leader" indicator
    to know if the CPU with the blocked donor is the one with the highest
    priority task. If the rq contains the highest priority task, it is
    that rq's pick which dictates the core-cookie.
    
    Once the find_proxy_task() chain stabilizes on the rq (find_proxy_task()
    returns the valid lock owner queued on the same rq), set a second
    "rq->core_pick_blocked_donor" and retry the pick.
    
    The flattened flow looks as follows:
    
      donor = pick_next_task()
        rq_set_donor(rq, donor)
    
      if (donor->is_blocked)
         next = find_proxy_task()
    
      /*
       * next has a different cookie compared to doner and
       * rq->core_pick_leader is set.
       */
      sched_core_swap_task()
        rq->core_pick_blocked_donor = 1; /* rq->donor was the last pick */
        return RETRY_TASK;
    
      pick_next_task()
        donor = rq->donor;
        owner = NULL;
    
        if (rq->core_pick_blocked_donor)
          owner = find_proxy_task(donor);
          goto restart_multi;
    
        /*
         * Finish the core-pick and validate that this rq
         * is still the pick leaader.
         */
        if (rq->core_pick_blocked_donor &&
            rq->core_pick_leader &&
            rq->core_pick == donor)
          rq->core_pick = owner; /* Swapped donor with owner. */
    
        rq->core->coore_cookie = rq->core_pick->cookie; /* owner cookie */
    
        /* Continue with the rest */
    
        return rq->core_pick; /* Owner */
    
        if (sched_core_retain_donor(rq))
           rq->sched_core_proxy_pick = 0 /* Consume */
    
    Since the owner is not blocked, it continues to run and rq->donor is
    preserved while the "rq->core_pick_blocked_donor" gets consumed.
    
    Signed-off-by: K Prateek Nayak <kprateek.nayak@amd.com>
    [jstultz: folded in follow-on fix from K Prateek, as well as small
     build fixes for !CONFIG_SCHED_PROXY_EXEC cases]
    Signed-off-by: John Stultz <jstultz@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3795,6 +3795,8 @@ static inline void ttwu_do_wakeup(struct task_struct *p)
 }
 
 #ifdef CONFIG_SCHED_PROXY_EXEC
+static struct task_struct *find_proxy_task(struct rq *rq, struct task_struct *donor,
+					   struct rq_flags *rf);
 static void proxy_deactivate(struct rq *rq, struct task_struct *donor);
 static void zap_balance_callbacks(struct rq *rq);
 
@@ -6555,9 +6557,15 @@ extern void task_vruntime_update(struct rq *rq, struct task_struct *p, bool in_f
 
 static void queue_core_balance(struct rq *rq);
 
+static __always_inline bool sched_core_proxy_pick(struct rq *rq)
+{
+	return sched_proxy_exec() && unlikely(rq->core_pick_blocked_donor);
+}
+
 static struct task_struct *
 pick_next_task(struct rq *rq, struct rq_flags *rf)
 {
+	struct task_struct *owner = NULL, *donor = rq->donor;
 	struct task_struct *next, *p, *max;
 	const struct cpumask *smt_mask;
 	bool fi_before = false;
@@ -6639,6 +6647,24 @@ pick_next_task(struct rq *rq, struct rq_flags *rf)
 	 */
 	rq->core->core_task_seq++;
 
+	/* Last core_pick resolved to a blocked_donor! */
+	if (sched_core_proxy_pick(rq)) {
+		WARN_ON_ONCE(!donor->is_blocked);
+
+		donor->blocked_donor = NULL;
+		owner = find_proxy_task(rq, donor, rf);
+		if (owner && owner != rq->idle)
+			goto restart_multi;
+		/*
+		 * Something changed in the proxy chain!
+		 * Retry pick like normal. Increment core_task_seq
+		 * since the core-wide lock might have been dropped
+		 * during proxy-migration in find_proxy_task().
+		 */
+		rq->core_pick_blocked_donor = false;
+		rq->core->core_task_seq++;
+	}
+
 	/*
 	 * Optimize for common case where this CPU has no cookies
 	 * and there are no cookied tasks running on siblings.
@@ -6697,6 +6723,28 @@ restart_multi:
 	}
 
 	rq_max->core_pick_leader = true;
+
+	if (sched_core_proxy_pick(rq)) {
+		WARN_ON_ONCE(!owner);
+
+		/*
+		 * Core-wide pick resolved to the same state as last time.
+		 * Swap the donor with the lock owner and continue the
+		 * rest of the pick sequence.
+		 */
+		if (rq->core_pick_leader && rq->core_pick == donor) {
+			rq_max->core_pick = owner;
+			max = owner;
+		} else {
+			/*
+			 * Pick resolved to a different leader / donor task.
+			 * Clear the "core_pick_blocked_donor" indicator and
+			 * proceed like normal.
+			 */
+			rq->core_pick_blocked_donor = false;
+		}
+	}
+
 	cookie = rq->core->core_cookie = max->core_cookie;
 
 	/*
@@ -6794,9 +6842,16 @@ restart_multi:
 	}
 
 out_set_next:
-	put_prev_set_next_task(rq, rq->donor, next);
-	if (rq->core->core_forceidle_count && next == rq->idle)
-		queue_core_balance(rq);
+	/*
+	 * If this is a redo for setting the owner's core-cookie,
+	 * put_prev_set_next_task() was already done during the
+	 * last pick and there is nothing to do now.
+	 */
+	if (!sched_core_proxy_pick(rq)) {
+		put_prev_set_next_task(rq, rq->donor, next);
+		if (rq->core->core_forceidle_count && next == rq->idle)
+			queue_core_balance(rq);
+	}
 
 	return next;
 }
@@ -7024,9 +7079,19 @@ sched_core_swap_pick(struct rq *rq, struct task_struct *next)
 		return rq->idle;
 	}
 
-	clear_task_blocked_on(rq->donor, NULL);
-	proxy_deactivate(rq, rq->donor);
+	rq->core_pick_blocked_donor = true;
 	return RETRY_TASK;
+}
+
+static bool sched_core_retain_donor(struct rq *rq)
+{
+	bool retain = rq->core_pick_blocked_donor;
+
+	if (!sched_core_enabled(rq))
+		return false;
+
+	rq->core_pick_blocked_donor = false;
+	return retain;
 }
 
 #else /* !CONFIG_SCHED_CORE: */
@@ -7051,6 +7116,11 @@ sched_core_swap_pick(struct rq *rq, struct task_struct *next)
 	 */
 	BUG();
 	return next;
+}
+
+static bool sched_core_retain_donor(struct rq *rq)
+{
+	return false;
 }
 
 #endif /* !CONFIG_SCHED_CORE */
@@ -7601,8 +7671,11 @@ pick_again:
 	if (sched_proxy_exec()) {
 		struct task_struct *prev_donor = rq->donor;
 
-		rq_set_donor(rq, next);
-		next->blocked_donor = NULL;
+		if (!sched_core_retain_donor(rq)) {
+			rq_set_donor(rq, next);
+			next->blocked_donor = NULL;
+		}
+
 		if (unlikely(next->is_blocked)) {
 			next = find_proxy_task(rq, next, &rf);
 			if (!next) {
