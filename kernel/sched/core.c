93256afddaf440e092310af1483f2a30dd45e65f
K Prateek Nayak <kprateek.nayak@amd.com>
2026-07-17 11:16:00 +0000

    sched/core: Fix forceidle when lock owner has mismatchd cookie
    Move Vasily's fix from 7cd4088f70e0 ("sched/core: Don't proxy-exec
    unmatched cookie lock owners") into a separate helper and fix the
    forceidle stats, balance callback queuing when rq is not the one
    dictating the core cookie.
    
    The helper will be extended in subsequent commits to allow for re-pick
    with cookie from the lock owner if rq is the pick leader.
    
    Signed-off-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Signed-off-by: John Stultz <jstultz@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3827,6 +3827,7 @@ void update_rq_avg_idle(struct rq *rq)
 }
 
 #ifdef CONFIG_SCHED_PROXY_EXEC
+static void proxy_deactivate(struct rq *rq, struct task_struct *donor);
 static void zap_balance_callbacks(struct rq *rq);
 
 static inline void proxy_reset_donor(struct rq *rq)
@@ -7045,6 +7046,23 @@ static inline void sched_core_cpu_dying(unsigned int cpu)
 		rq->core = rq;
 }
 
+static struct task_struct *
+sched_core_swap_pick(struct rq *rq, struct task_struct *next)
+{
+	if (!rq->core_pick_leader) {
+		if (!rq->core->core_forceidle_count++)
+			rq->core->core_forceidle_seq++;
+
+		proxy_resched_idle(rq);
+		queue_core_balance(rq);
+		return rq->idle;
+	}
+
+	clear_task_blocked_on(rq->donor, NULL);
+	proxy_deactivate(rq, rq->donor);
+	return RETRY_TASK;
+}
+
 #else /* !CONFIG_SCHED_CORE: */
 
 static inline void sched_core_cpu_starting(unsigned int cpu) {}
@@ -7056,6 +7074,18 @@ pick_next_task(struct rq *rq, struct rq_flags *rf)
 	__must_hold(__rq_lockp(rq))
 {
 	return __pick_next_task(rq, rf);
+}
+
+static struct task_struct *
+sched_core_swap_pick(struct rq *rq, struct task_struct *next)
+{
+	/*
+	 * Caller should always check !sched_cpu_cookie_match(rq, next)
+	 * which is always false for !CONFIG_SCHED_CORE and execution
+	 * should never reach here.
+	 */
+	BUG();
+	return next;
 }
 
 #endif /* !CONFIG_SCHED_CORE */
@@ -7459,13 +7489,6 @@ find_proxy_task(struct rq *rq, struct task_struct *donor, struct rq_flags *rf)
 	}
 	WARN_ON_ONCE(owner && !owner->on_rq);
 
-	if (owner && !sched_cpu_cookie_match(rq, owner)) {
-		if (curr_in_chain)
-			return proxy_resched_idle(rq);
-		p = donor; /* Deactivate the donor, not the runnable owner */
-		clear_task_blocked_on(p, NULL);
-		goto deactivate;
-	}
 	return owner;
 
 deactivate:
@@ -7627,6 +7650,11 @@ pick_again:
 			if (next == rq->idle) {
 				zap_balance_callbacks(rq);
 				goto keep_resched;
+			}
+			if (!sched_cpu_cookie_match(rq, next)) {
+				next = sched_core_swap_pick(rq, next);
+				if (next == RETRY_TASK)
+					goto pick_again;
 			}
 		}
 		if (rq->donor == prev_donor && prev != next) {
